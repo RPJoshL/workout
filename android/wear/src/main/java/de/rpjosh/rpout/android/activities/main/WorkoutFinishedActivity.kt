3580f4adc4e7338b4bbddf8b71f17c57ce61acec
RPJosh <RPJosh@rpjosh.de>
2024-11-20 16:56:22 +0100

    Add communication with RPdb

--- android/wear/src/main/java/de/rpjosh/rpout/android/activities/main/WorkoutFinishedActivity.kt
+++ android/wear/src/main/java/de/rpjosh/rpout/android/activities/main/WorkoutFinishedActivity.kt
@@ -91,6 +91,7 @@ import de.rpjosh.rpout.android.shared.controller.MetricController
 import de.rpjosh.rpout.android.shared.controller.WorkoutController
 import de.rpjosh.rpout.android.shared.helper.Helper
 import de.rpjosh.rpout.android.shared.inject.Inject
+import de.rpjosh.rpout.android.shared.customizations.RPdbBroadcaster
 import de.rpjosh.rpout.android.shared.models.GpsWorkout
 import de.rpjosh.rpout.android.shared.models.HeartRateZone
 import de.rpjosh.rpout.android.shared.models.WorkoutSummary
@@ -164,6 +165,8 @@ class WorkoutFinishedActivity: ComponentActivity() {
 
     private lateinit var manager: WorkoutManager
     private var serviceStopped: Boolean = false
+    
+    private lateinit var rpdbCommunication: RPdbBroadcaster
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
@@ -185,6 +188,7 @@ class WorkoutFinishedActivity: ComponentActivity() {
 
         Singleton.appController.injection.inject(WorkoutFinishedActivity::class.java, null, false, this)
         connectivityManager = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        rpdbCommunication = RPdbBroadcaster(this)
 
         // Do not turn display off
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
@@ -227,6 +231,9 @@ class WorkoutFinishedActivity: ComponentActivity() {
                 finish()
                 return@launch
             }
+
+            // Request network
+            rpdbCommunication.requestConnectivity("WORKOUT_END", 300)
 
             // Limit max data points to 900 for uploading a workout over bluetooth (this  will take ~30 seconds)
             val wifiRequired = workout.points.size > 900
@@ -383,6 +390,8 @@ class WorkoutFinishedActivity: ComponentActivity() {
         connectivityManager.bindProcessToNetwork(null)
         if (::networkCallback.isInitialized) connectivityManager.unregisterNetworkCallback(networkCallback)
 
+        // Stop requested network connectivity
+        rpdbCommunication.dropConnectivity("WORKOUT_END")
 
         // Push a sync job if it wasn't done already (activity was exited immediately before the initial push wasn't even tried)
         if (pushSyncJobOnExit.get()) {
--- android/wear/src/main/java/de/rpjosh/rpout/android/workout/WorkoutManager.kt
+++ android/wear/src/main/java/de/rpjosh/rpout/android/workout/WorkoutManager.kt
@@ -41,6 +41,7 @@ import androidx.health.services.client.resumeExercise
 import androidx.health.services.client.startExercise
 import de.rpjosh.rpout.android.shared.R
 import de.rpjosh.rpout.android.shared.controller.WorkoutController
+import de.rpjosh.rpout.android.shared.customizations.RPdbBroadcaster
 import de.rpjosh.rpout.android.shared.helper.TimeHelper
 import de.rpjosh.rpout.android.shared.inject.Inject
 import de.rpjosh.rpout.android.shared.inject.InjectionFactory
@@ -122,6 +123,9 @@ class WorkoutManager(private val typeId: Long) {
     lateinit var phoneTracking: PhoneTracking
         private set
 
+    /** Communication with RPdb */
+    private lateinit var rpdbCommunication: RPdbBroadcaster
+
     companion object {
 
         /** Global state of the workout manager */
@@ -177,7 +181,6 @@ class WorkoutManager(private val typeId: Long) {
     /** Starts the (already prepared) workout with the exercise client */
     @SuppressLint("RestrictedApi")
     suspend fun start() {
-
         // Check if the workout is currently running
         healthExerciseClient?.let {
             try {
@@ -199,6 +202,8 @@ class WorkoutManager(private val typeId: Long) {
                 logger.log("w", ex, "Failed to get current exercise status")
             }
         }
+        
+        rpdbCommunication.startWorkout()
 
         // Create workout struct to sync against the server
         synchronized(dataLock) {
@@ -265,6 +270,7 @@ class WorkoutManager(private val typeId: Long) {
     /** Pauses the currently running workout */
     suspend fun pause() {
         logger.log("i", "Paused workout (#${gpsWorkout.id})")
+        rpdbCommunication.stopWorkout()
 
         healthExerciseClient?.pauseExercise()
         phoneTracking.pauseExercise()
@@ -277,6 +283,7 @@ class WorkoutManager(private val typeId: Long) {
     /** Resumes the workout from a paused workout */
     suspend fun resume() {
         logger.log("i", "Resumed workout (#${gpsWorkout.id})")
+        rpdbCommunication.startWorkout()
 
         healthExerciseClient?.resumeExercise()
         phoneTracking.resumeExercise()
@@ -318,6 +325,7 @@ class WorkoutManager(private val typeId: Long) {
 
         // Wait until workout is completely processed
         healthExerciseClient?.endExercise()
+        rpdbCommunication.stopWorkout()
         select {
             endChannel.onReceive {
                // Continue processing
@@ -677,6 +685,9 @@ class WorkoutManager(private val typeId: Long) {
         }
         if (!::notificationManager.isInitialized) {
             notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        }
+        if (!::rpdbCommunication.isInitialized) {
+            rpdbCommunication = RPdbBroadcaster(context)
         }
 
         val callback = object : ExerciseUpdateCallback {
